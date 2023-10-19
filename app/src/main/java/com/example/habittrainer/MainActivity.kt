400b90c0009abb1f1176b985adcb391aa5002a0a
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-19 10:08:03 -0600

    Kotlin Fundamentals - First Commit

--- app/src/main/java/com/example/habittrainer/MainActivity.kt
+++ app/src/main/java/com/example/habittrainer/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iv_icon.setImageResource(R.drawable.water)
+        tv_title.text = getString(R.string.drink_water)
+        tv_description.text = getString(R.string.drink_water_desc)
+
     }
 }
