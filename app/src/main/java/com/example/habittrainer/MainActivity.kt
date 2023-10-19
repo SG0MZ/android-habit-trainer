385450619104d5e870c466e7234bd60b4364dbc8
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-19 11:45:08 -0600

    User Create Habits - First Commit

--- app/src/main/java/com/example/habittrainer/MainActivity.kt
+++ app/src/main/java/com/example/habittrainer/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.habittrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,22 @@ class MainActivity : AppCompatActivity() {
 
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = HabitsAdapter(getSampleHabits())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == androidx.appcompat.R.id.add_habit) {
+            switchTo(CreateHabitActivity::class.java)
+        }
+        return true
+    }
+
+    private fun switchTo(c: Class<*>) {
+        val intent = Intent(this,c)
+        startActivity(intent)
     }
 }
