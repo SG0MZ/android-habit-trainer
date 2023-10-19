537c2d0c9b7ff0822972db9f02f7cbde7b613327
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-19 10:27:15 -0600

    RecyclerView - First Commit

---
+++ app/src/main/java/com/example/habittrainer/HabitsAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.habittrainer
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HabitsAdapter(val habits: List<Habit>) : RecyclerView.Adapter<HabitsAdapter.HabitViewHolder>() {
+    class HabitViewHolder(val iv: View) : RecyclerView.ViewHolder(iv)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card,parent)
+
+        return HabitViewHolder(view)
+    }
+
+    override fun getItemCount() = habits.size
+
+    override fun onBindViewHolder(holder: HabitViewHolder, index: Int) {
+        if (holder != null) {
+            val habit = habits[index]
+            holder.card.tv_title.text = habit.title
+            holder.card.tv_description.text = habit.description
+            holder.card.tv_icon.setImageResource(habit.image)
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/habittrainer/MainActivity.kt
+++ app/src/main/java/com/example/habittrainer/MainActivity.kt
@@ -2,15 +2,16 @@ package com.example.habittrainer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        iv_icon.setImageResource(R.drawable.water)
-        tv_title.text = getString(R.string.drink_water)
-        tv_description.text = getString(R.string.drink_water_desc)
+        rv.setHasFixedSize(true)
 
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = HabitsAdapter(getSampleHabits())
     }
 }
